7b5e533e15328f566f05af7bfca535dab7e394e6
Mirchev <Mirchev@MIRCHEV-PC>
2017-05-18 19:05:15 +0300

    Initial Dark Souls commit

---
+++ DarkSoulsSiteMVC/DarkSoulsSiteMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DarkSoulsSiteMVC.MvcApplication" Language="C#" %>
---
+++ DarkSoulsSiteMVC/DarkSoulsSiteMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DarkSoulsSiteMVC.Startup))]
+namespace DarkSoulsSiteMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
